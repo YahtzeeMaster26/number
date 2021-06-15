41ce87537c16c2ca105b27d163a0c45974e59c17
NoHaxJustPi <68925272+NoHaxJustPi@users.noreply.github.com>
2021-06-15 02:38:46 -0500

    gender

---
+++ gender.js
@@ -0,0 +1,11 @@
+//never gonna give you up, get rique rolt!
+//shoutouts to simpleflips btw
+//callout post: NoHaxJustPi#5200 on discord for writing stupid comments in their code
+
+gender = "?";
+function gameLoop() {
+    gender += "?";
+    document.getElementById("gender").innerHTML = gender;
+}
+
+setInterval(gameLoop, 1000);
